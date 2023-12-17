f1bb76d1e14856a61c107712adf10f0013c08889
omerSungur <omersungur615@gmail.com>
2023-12-17 23:04:17 +0300

    Create pass function

--- app/src/main/java/com/omersungur/theworldwander/presentation/Screen.kt
+++ app/src/main/java/com/omersungur/theworldwander/presentation/Screen.kt
@@ -1,5 +1,7 @@
 package com.omersungur.theworldwander.presentation
 
 sealed class Screen(val route: String) {
-    data object CountryListScreen : Screen("country_list_screen")
+    data object CountryListScreen : Screen("country_list_screen?countryId={countryId}") {
+        fun passCountryId(countryId: String) = "country_list_screen?countryId=$countryId"
+    }
 }
