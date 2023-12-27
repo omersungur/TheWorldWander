5ac109c5f116117b6c532918c671230b568dd858
omerSungur <omersungur615@gmail.com>
2023-12-27 13:16:08 +0300

    Add constants for data store and database

--- app/src/main/java/com/omersungur/theworldwander/core/Constants.kt
+++ app/src/main/java/com/omersungur/theworldwander/core/Constants.kt
@@ -3,4 +3,15 @@ package com.omersungur.theworldwander.core
 object Constants {
 
     const val BASE_URL = "https://restcountries.com/v2/"
+    const val APP_ID = "myworldwanderapp-qurhy"
+    const val CLIENT_ID = "936890598010-npto9631o04518u0vo0bgp33cvvfbb1m.apps.googleusercontent.com"
+
+    const val COUNTRY_DETAIL_SCREEN_ARGUMENT_ID = "countryId"
+
+    const val PREFERENCES_NAME = "countries_preferences"
+    const val PREFERENCES_GET_DATA_FOR_THE_FIRST_TIME = "getDataFromApi"
+
+    const val IMAGES_DATABASE = "images_db"
+    const val IMAGE_TO_UPLOAD_TABLE = "image_to_upload_table"
+    const val IMAGE_TO_DELETE_TABLE = "image_to_delete_table"
 }
