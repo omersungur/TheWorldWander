0951141c05cbcd662b082bc3817bca5704dfe0d0
omerSungur <omersungur615@gmail.com>
2023-12-27 13:18:29 +0300

    Add detail screen route

--- app/src/main/java/com/omersungur/theworldwander/presentation/Screen.kt
+++ app/src/main/java/com/omersungur/theworldwander/presentation/Screen.kt
@@ -1,7 +1,12 @@
 package com.omersungur.theworldwander.presentation
 
+import com.omersungur.theworldwander.core.Constants.COUNTRY_DETAIL_SCREEN_ARGUMENT_ID
+
 sealed class Screen(val route: String) {
-    data object CountryListScreen : Screen("country_list_screen?countryId={countryId}") {
-        fun passCountryId(countryId: String) = "country_list_screen?countryId=$countryId"
+    data object AuthScreen : Screen("authentication_screen")
+    data object CountryListScreen : Screen("country_list_screen")
+    data object CountryDetailScreen: Screen("country_detail_screen?$COUNTRY_DETAIL_SCREEN_ARGUMENT_ID={$COUNTRY_DETAIL_SCREEN_ARGUMENT_ID}") {
+        fun passCountryId(countryId: String) =
+            "country_detail_screen?$COUNTRY_DETAIL_SCREEN_ARGUMENT_ID=$countryId"
     }
 }
