9372c57429f4f8553639eaf0df15e8f612cda524
omerSungur <omersungur615@gmail.com>
2023-12-27 13:50:22 +0300

    Add gallery component

---
+++ app/src/main/java/com/omersungur/theworldwander/presentation/screen_country_list/components/Gallery.kt
@@ -0,0 +1,102 @@
+package com.omersungur.theworldwander.presentation.screen_country_list.components
+
+import android.net.Uri
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerBasedShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+
+@Composable
+fun Gallery(
+    modifier: Modifier = Modifier,
+    images: List<Uri>,
+    imageSize: Dp = 40.dp,
+    spaceBetweenImages: Dp = 10.dp,
+    imageShape: CornerBasedShape = Shapes().small
+) {
+    BoxWithConstraints(modifier = modifier) {
+        val numberOfVisibleImages = remember {
+            derivedStateOf {
+                this.maxWidth.div(spaceBetweenImages + imageSize).toInt().minus(1)
+            }
+        }
+
+        val remainingImages = remember {
+            derivedStateOf {
+                images.size - numberOfVisibleImages.value
+            }
+        }
+
+        Row {
+            images.take(numberOfVisibleImages.value).forEach { image ->
+                AsyncImage(
+                    modifier = Modifier
+                        .clip(imageShape)
+                        .size(imageSize),
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(image)
+                        .crossfade(true)
+                        .build(),
+                    contentScale = ContentScale.Crop,
+                    contentDescription = "Gallery Image"
+                )
+                Spacer(modifier = Modifier.width(spaceBetweenImages))
+            }
+
+            if (remainingImages.value > 0) {
+                LastImageOverlay(
+                    imageSize = imageSize,
+                    imageShape = imageShape,
+                    remainingImages = remainingImages.value
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun LastImageOverlay(
+    imageSize: Dp,
+    imageShape: CornerBasedShape,
+    remainingImages: Int
+) {
+    Box(contentAlignment = Alignment.Center) {
+        Surface(
+            modifier = Modifier
+                .clip(imageShape)
+                .size(imageSize),
+            color = MaterialTheme.colorScheme.primaryContainer
+        ) {
+
+        }
+        Text(
+            text = "+$remainingImages",
+            style = TextStyle(
+                fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+                fontWeight = FontWeight.Medium
+            ),
+            color = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    }
+}
