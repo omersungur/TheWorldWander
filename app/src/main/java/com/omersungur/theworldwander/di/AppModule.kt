52894218e1a43721cd508ed22fac2b810922f8f1
omerSungur <omersungur615@gmail.com>
2023-12-27 13:13:35 +0300

    Add app module for repo and use case

--- app/src/main/java/com/omersungur/theworldwander/di/AppModule.kt
+++ app/src/main/java/com/omersungur/theworldwander/di/AppModule.kt
@@ -1,10 +1,21 @@
 package com.omersungur.theworldwander.di
 
+import com.omersungur.theworldwander.data.local.database.ImageToDeleteDao
+import com.omersungur.theworldwander.data.local.database.ImageToUploadDao
 import com.omersungur.theworldwander.data.remote.CountryApi
 import com.omersungur.theworldwander.data.repository.CountryRepositoryImpl
 import com.omersungur.theworldwander.domain.repository.CountryRepository
 import com.omersungur.theworldwander.domain.use_case.CountryUseCases
 import com.omersungur.theworldwander.domain.use_case.GetCountries
+import com.omersungur.theworldwander.domain.use_case.mongo.AddCountriesInMongoDB
+import com.omersungur.theworldwander.domain.use_case.mongo.AddImagesToFirebase
+import com.omersungur.theworldwander.domain.use_case.mongo.CountryMongoUseCases
+import com.omersungur.theworldwander.domain.use_case.mongo.DeleteAllImagesFromMongoDB
+import com.omersungur.theworldwander.domain.use_case.mongo.DeleteImagesFromFirebase
+import com.omersungur.theworldwander.domain.use_case.mongo.FilterCountries
+import com.omersungur.theworldwander.domain.use_case.mongo.GetAllCountriesFromMongoDB
+import com.omersungur.theworldwander.domain.use_case.mongo.GetSelectedCountry
+import com.omersungur.theworldwander.domain.use_case.mongo.UpdateCountryImage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -28,6 +39,24 @@ object AppModule {
     ): CountryUseCases {
         return CountryUseCases(
            getCountries = GetCountries(countryRepository)
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideCountryMongoUseCases(
+        imageToDeleteDao: ImageToDeleteDao,
+        imageToUploadDao: ImageToUploadDao
+    ): CountryMongoUseCases {
+        return CountryMongoUseCases(
+            addCountriesInMongoDB = AddCountriesInMongoDB(),
+            filterCountries = FilterCountries(),
+            getAllCountriesFromMongoDB = GetAllCountriesFromMongoDB(),
+            deleteAllImagesFromMongoDB = DeleteAllImagesFromMongoDB(imageToDeleteDao),
+            getSelectedCountry = GetSelectedCountry(),
+            addImagesToFirebase = AddImagesToFirebase(imageToUploadDao),
+            deleteImagesFromFirebase = DeleteImagesFromFirebase(imageToDeleteDao),
+            updateCountryImage = UpdateCountryImage()
         )
     }
 }
