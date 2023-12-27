c893721ae30ac63f114bec1f109d01ebc4be89e5
omerSungur <omersungur615@gmail.com>
2023-12-27 13:14:41 +0300

    Add zoom feature for image

---
+++ app/src/main/java/com/omersungur/theworldwander/presentation/screen_country_detail/components/ZoomableImage.kt
@@ -0,0 +1,89 @@
+package com.omersungur.theworldwander.presentation.screen_country_detail.components
+
+import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.omersungur.theworldwander.domain.model.GalleryImage
+
+@Composable
+fun ZoomableImage(
+    selectedGalleryImage: GalleryImage,
+    onCloseClicked: () -> Unit,
+    onDeleteClicked: () -> Unit
+) {
+    var offsetX by remember { mutableFloatStateOf(0f) }
+    var offsetY by remember { mutableFloatStateOf(0f) }
+    var scale by remember { mutableFloatStateOf(1f) }
+    Box(
+        modifier = Modifier
+            .pointerInput(Unit) {
+                detectTransformGestures { _, pan, zoom, _ ->
+                    scale = maxOf(1f, minOf(scale * zoom, 5f))
+                    val maxX = (size.width * (scale - 1)) / 2
+                    val minX = -maxX
+                    offsetX = maxOf(minX, minOf(maxX, offsetX + pan.x))
+                    val maxY = (size.height * (scale - 1)) / 2
+                    val minY = -maxY
+                    offsetY = maxOf(minY, minOf(maxY, offsetY + pan.y))
+                }
+            }
+    ) {
+        AsyncImage(
+            modifier = Modifier
+                .fillMaxSize()
+                .graphicsLayer(
+                    scaleX = maxOf(.5f, minOf(3f, scale)),
+                    scaleY = maxOf(.5f, minOf(3f, scale)),
+                    translationX = offsetX,
+                    translationY = offsetY
+                ),
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(selectedGalleryImage.image.toString())
+                .crossfade(true)
+                .build(),
+            contentScale = ContentScale.Fit,
+            contentDescription = "Gallery Image"
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp)
+                .padding(top = 24.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(onClick = onCloseClicked) {
+                Icon(imageVector = Icons.Default.Close, contentDescription = "Close Icon")
+                Text(text = "Close")
+            }
+            Button(onClick = onDeleteClicked) {
+                Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete Icon")
+                Text(text = "Delete")
+            }
+        }
+    }
+}
