8d2d7f88128276ad4fc2b4d684aa2235dda0cb8f
omerSungur <omersungur615@gmail.com>
2023-12-27 13:15:33 +0300

    Add extension functions for firebase

---
+++ app/src/main/java/com/omersungur/theworldwander/core/ExtFunction.kt
@@ -0,0 +1,75 @@
+package com.omersungur.theworldwander.core
+
+import android.net.Uri
+import android.util.Log
+import androidx.core.net.toUri
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.storageMetadata
+import com.omersungur.theworldwander.data.local.database.entity.ImageToDelete
+import com.omersungur.theworldwander.data.local.database.entity.ImageToUpload
+import io.realm.kotlin.types.RealmInstant
+import java.time.Instant
+
+fun Instant.toRealmInstant(): RealmInstant {
+    val sec: Long = this.epochSecond
+    val nano: Int = this.nano
+    return if (sec >= 0) {
+        RealmInstant.from(sec, nano)
+    } else {
+        RealmInstant.from(sec + 1, -1_000_000 + nano)
+    }
+}
+
+fun RealmInstant.toInstant(): Instant {
+    val sec: Long = this.epochSeconds
+    val nano: Int = this.nanosecondsOfSecond
+    return if (sec >= 0) {
+        Instant.ofEpochSecond(sec, nano.toLong())
+    } else {
+        Instant.ofEpochSecond(sec - 1, 1_000_000 + nano.toLong())
+    }
+}
+
+fun fetchImagesFromFirebase(
+    remoteImagePaths: List<String>,
+    onImageDownload: (Uri) -> Unit,
+    onImageDownloadFailed: (Exception) -> Unit = {},
+    onReadyToDisplay: () -> Unit = {}
+) {
+    if (remoteImagePaths.isNotEmpty()) {
+        remoteImagePaths.forEachIndexed { index, remoteImagePath ->
+            if (remoteImagePath.trim().isNotEmpty()) {
+                FirebaseStorage.getInstance().reference.child(remoteImagePath.trim()).downloadUrl
+                    .addOnSuccessListener {
+                        onImageDownload(it)
+                        if (remoteImagePaths.lastIndexOf(remoteImagePaths.last()) == index) {
+                            onReadyToDisplay()
+                        }
+                    }.addOnFailureListener {
+                        onImageDownloadFailed(it)
+                    }
+            }
+        }
+    }
+}
+
+fun retryUploadingImageToFirebase(
+    imageToUpload: ImageToUpload,
+    onSuccess: () -> Unit
+) {
+    val storage = FirebaseStorage.getInstance().reference
+    storage.child(imageToUpload.remoteImagePath).putFile(
+        imageToUpload.imageUrl.toUri(),
+        storageMetadata { },
+        imageToUpload.sessionUri.toUri()
+    ).addOnSuccessListener { onSuccess() }
+}
+
+fun retryDeletingImageFromFirebase(
+    imageToDelete: ImageToDelete,
+    onSuccess: () -> Unit
+) {
+    val storage = FirebaseStorage.getInstance().reference
+    storage.child(imageToDelete.remoteImagePath).delete()
+        .addOnSuccessListener { onSuccess() }
+}
